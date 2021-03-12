0455ff81ae6cbcde1d08165655bacd2ac0a22297
Pradeep Jerome <prjerome@gmail.com>
2021-03-12 15:18:12 +0100

    Ran rubocop -A; added home page link for sign up page; changed sign up page view; changed sigup routing; added integration test for signup page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,10 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
+  test 'should get new' do
+    get signup_path
     assert_response :success
   end
 end
