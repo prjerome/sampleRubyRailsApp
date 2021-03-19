e5b7b6bdcd2d87121623fa509247764d2937f8e8
Pradeep Jerome <prjerome@gmail.com>
2021-03-19 12:16:20 +0100

    Added user model email validations, via TDD; added unqiue database index for email addresses; removed users.yml fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -44,8 +44,13 @@ class UserTest < ActiveSupport::TestCase
   end
   test 'users with the same email address should be rejected' do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+  test 'email addresses should be saved as lower case' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
