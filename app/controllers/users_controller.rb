068c403fa0328c5c104fab9c4dbf0c5aff5a6812
Pradeep Jerome <prjerome@gmail.com>
2021-03-22 17:42:31 +0100

    Added  sign up form view and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def show
     @user = User.find(params[:id])
+    
   end
 end
