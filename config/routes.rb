d11e10b89caa85084776c4995389ef32b6ba0cee
Pradeep Jerome <prjerome@gmail.com>
2021-03-24 16:29:20 +0100

    Added Sessions resource; changed config/routes.rb to include Sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
