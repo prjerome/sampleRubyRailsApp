533d3257590ca36a488985a74c3a36d4e8ffbe17
Pradeep Jerome <prjerome@gmail.com>
2021-03-25 18:12:46 +0100

    Created Sessions resource for login;added SessionsHelper module to ApplicationController class, thereby all controllers, which inherit from AppplicationController, have access to the methods within the SessionsHelper module; added login view using the form_with helper method; added integration test for invalid login flow

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,8 +6,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', help_path
-    assert_select 'a[href=?]', about_path
-    assert_select 'a[href=?]', contact_path
     get contact_path
     assert_select 'title', full_title('Contact')
     get signup_path 
