8d532f73255e2f996af6d6dc35de7f4f06a09c56
Pradeep Jerome <prjerome@gmail.com>
2021-03-25 18:20:42 +0100

    Added currently logged-in-user retrieval method, based on current browser session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,14 @@
 module SessionsHelper
-  
   # Logs in the given user
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  # Returns the current logged in user
+  def current_user
+    if sessions[:user_id]
+       @current_user ||= User.find_by(id: [session[:user_id]])
+    end
+    
   end
 end
